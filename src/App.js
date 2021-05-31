14d5b34a934e5e7e72c91964c5a64ab96c5b1a03
Emily Nakashima <emily@honeycomb.io>
2021-05-31 12:16:08 -0700

    Add libhoney so we can send telemetry to Honeycomb

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ import Item from "./components/Item";
 import Search from "./components/Search";
 import NotFound from "./components/NotFound";
 
+// Libhoney is the Honeycomb SDK for sending telemetry Events to Honeycomb's API
+// Docs for Libhoney live at https://docs.honeycomb.io/getting-data-in/javascript/libhoney/
+// Here's our Honeycomb SDK configuration
+import Libhoney from "libhoney"; 
+
+let hny = new Libhoney({
+  writeKey: "YOUR_API_KEY",
+  dataset: "snapshop-app-events"
+});
+
+// Let's send an event each time the app starts up
+let ev = hny.newEvent();
+ev.addField("event_type", "startup");
+// Uncomment this line to add a user agent field to your Events
+// ev.addField("user_agent", typeof window && window.navigator && window.navigator.userAgent );
+ev.send();
+
+// And here's our actual app code:
 class App extends Component {
   // Prevent page reload, clear input, set URL and push history on submit
   handleSubmit = (e, history, searchInput) => {
